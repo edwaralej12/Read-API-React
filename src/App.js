5644600cf211f740b1fc58c7597266f5b7556216
Edwar Gutierrez <edwaralejandro595@gmail.com>
2020-05-01 09:38:02 -0500

    Se crea el proyecto de practica de como consumir API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Loading from './components/Loading';
+import './styles/App.css';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: true,
+      error: null,
+      data: {}
+
+    }
+  }
+
+  _fetchData() {
+    Axios.get('https://swapi.dev/api/vehicles/')
+      .then(res => {
+        const vehicleData = res.data.results;
+        console.log(vehicleData);
+        this.setState(
+          {
+            loading: false,
+            data: vehicleData,
+          });
+      })
+      .catch((error) => {
+        this.setState(
+          {
+            loading: false,
+            error : isNaN
+            
+          }
+        )
+      });
+  }
+
+  componentDidMount() {
+    this._fetchData();
+
+  }
+
+
+  render() {
+
+    if (this.state.loading) {
+      return (
+        <div className='App'>
+          <Loading />
+        </div>
+      );
+    }
+
+    if (this.state.error !== null) {
+      return <h1>Error</h1>
+    }
+
+    return (
+      <div className="App">
+        <h1>Ciclo de vida </h1>
+        <h2>nombre del personaje del estado</h2>
+        <ul>
+          {this.state.data.map((character) => (
+            <li>{character.name}</li>
+          ))}
+
+        </ul>
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
---
+++ src/components/Loading.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './styles/Loading.css';
+
+class Loading extends Component {
+    render() {
+        return (
+            <div className="lds-dual-ring"></div>
+            
+        );
+    }
+}
+
+export default Loading;
